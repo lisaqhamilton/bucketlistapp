462f12058e5959b0b9e5932bad65c56b943e57ea
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-20 16:37:26 -0500

    creating passport

--- server/router.js
+++ server/router.js
@@ -1,6 +1,15 @@
 var Auth = require('./controllers/auth');
-var User = require('./models/user');
+var passportService = require('./services/passport');
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app) {
+
+	app.get('/', requireAuth, function(req, res) {
+		res.send('Holla!');
+
+	});
+	
 	app.post('/signup', Auth.signup);
 }
