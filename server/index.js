ec81681bd81c2307115955e260c322252fa5c9d1
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-20 13:15:02 -0500

    middleware changes

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json({ type: '*/*'}));
+
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+
+server.listen(port);
+console.log('Yo, I\'m listening up in here on port ' + port);
