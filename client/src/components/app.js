91677b447ecaabf85515bba441be66b3bf2e6d6a
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-21 16:14:35 -0500

    components examples

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-	export default class App extends Component {
-		render() {
-			return (
-				<div>React Hello World</div>
-				);
+
+	import JSXIntro from './1_JSXIntro';
+	import ComponentsIntro from './2_ComponentsIntro.js';
+	import PropsIntro from './3_PropsIntro.js';
+
+		export default class App extends Component {
+			render() {
+				return (
+					<div>
+						<h1>React Intro</h1>
+						<JSXIntro />
+						<ComponentsIntro />
+						<PropsIntro name="Michael Bolton" title="The Neverending Elevator Music" />
+
+					</div>
+					);
+			}
 		}
-	}
