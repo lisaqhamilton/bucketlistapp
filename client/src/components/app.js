38aa8df754f87428c650bdcea543ebd8d4f781c3
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-22 14:26:01 -0500

    added searchbar component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
 import NavBarHeader from './nav';
+import Video from './video/video';
+import SearchBar from './video/search-bar';
 
 	export default class App extends Component {
 		render() {
 			return (
 				<div>
 					<NavBarHeader />
+					<Video />
 				</div>
 				);
 		}
---
+++ client/src/components/video/video.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search-bar';
+import VideoDetail from './video-detail';
+
+const API_Key = 'AIzaSyCHiqMgJetwdpYMYJNJlugZSXU_OB-5NF8';
+
+class Video extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('bucketlist');
+	}
+	videoSearch(term) {
+		YTSearch({key: API_Key, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+	render() {
+		const videoSearch = _.debounce((term)=>{ this.videoSearch(term) }, 300);
+		return (
+			<div>
+				<SearchBar on SearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+			</div>
+			);
+	}
+}
+
+export default Video;
