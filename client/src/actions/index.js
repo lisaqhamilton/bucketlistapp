c3b0f6d9cf179c872f3e08af0a7d63af28bdbbf2
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-23 15:36:21 -0500

    authreducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-
-
-// const ROOT_URL = 'http://rest.learncode.academy/api/lisabucketlist';
-const ROOT_URL = 'http://localhost:3000';
+import { AUTH_USER, UNAUTH_USER } from './types';
+import authReducer from '../reducers/auth_reducer';
 
 export const CREATE_POSTS = 'CREATE_POSTS';
-
+// const ROOT_URL = 'http://rest.learncode.academy/api/lisabucketlist';
+const ROOT_URL = 'http://localhost:3000';
 
 export function signinUser({ email, password }) {
 	return function(dispatch) {
 		axios.post(`${ROOT_URL}/signin`, { email, password })
 		.then(response => {
+			//This only kickstarts if the request was good...
+			//We now update the state to indicate authenticated user
+			dispatch({ type: AUTH_USER });
+			//This will put the token in localStorage. It's safe!!
+			localStorage.setItem('token', response.data.token);
+			//This sends us off to the /newitem view
 			browserHistory.push('/newitem');
 		})
 		.catch(() => {
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,14 @@
 import { reducer as formReducer } from 'redux-form';
 import { combineReducers } from 'redux';
+import authReducer from './auth_reducer';
+import {
+	AUTH_USER,
+	UNAUTH_USER
+} from '../actions/types';
 	
 	const rootReducer = combineReducers({
-		form: formReducer
+		form: formReducer,
+		auth: authReducer
 	});
 
 	export default rootReducer;
