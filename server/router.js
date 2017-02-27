d3c227dade442ff77e3ae33e050c902e8cc117d6
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-02-27 15:54:04 -0500

    fetchdeleteitems

--- server/router.js
+++ server/router.js
@@ -16,5 +16,9 @@ module.exports = function(app) {
 	app.post('/newitem', requireAuth, BucketList.addBucketList);
 
 	app.get('/items', requireAuth, BucketList.fetchBucketLists);
+
+	app.get('/items/:id', requireAuth, BucketList.fetchBucketList);
+
+	app.delete('/items/:id', requireAuth, BucketList.deleteBucketList);
 	
 }
